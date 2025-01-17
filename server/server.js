85f6310c5463234ae56d998ca88fcd50ae90da9e
NikhilSai5 <nikhilsaimanam5@gmail.com>
2025-01-17 18:06:56 +0530

    data added to the database

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) =>
+    console.error("Error connecting to MongoDB:", error.message)
+  );
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`the server is running on http://localhost:${port}`);
+  console.log(`The server is running on http://localhost:${port}`);
 });
