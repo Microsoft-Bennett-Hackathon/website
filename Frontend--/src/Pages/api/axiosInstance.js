20f8148b55ad1220b6dd8a6b1fe557173c1a8410
Your Name <your.email@example.com>
2025-01-20 10:45:36 +0530

    removed the unwanted folder

--- Frontend--/src/Pages/api/axiosInstance.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:5001', // Point to Flask backend on port 5001
-});
-
-export default axiosInstance;
