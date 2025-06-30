993b659742e32eb7acb600c891d1c8d4250b6705
Siddhesh Chawande <145762207+sidd0000@users.noreply.github.com>
2025-06-30 16:25:06 +0530

    Update check.py

--- chatbot/check.py
+++ chatbot/check.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key = 'AIzaSyChLYW845Nu3kTrgizXvztf1Uxr12S32HE')
+genai.configure(api_key = '')
 
 model = genai.GenerativeModel(model_name="tunedModels/jaatram-b0u9xa08zbp8")
 result = model.generate_content("How is politiccs in india?")
