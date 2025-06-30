92f8dd8f1e9ca45f8dde9d8b00fb5f25ed216a43
Siddhesh Chawande <145762207+sidd0000@users.noreply.github.com>
2025-06-30 16:25:21 +0530

    Update version.py

--- chatbot/version.py
+++ chatbot/version.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key = 'AIzaSyChLYW845Nu3kTrgizXvztf1Uxr12S32HE')
+genai.configure(api_key = '')
 
 for model_info in genai.list_tuned_models():
     print(model_info.name)
